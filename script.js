1f3ab861f260fa7255fcbff21af2c1de71fbbdbc
technoeagle20 <sushreesmohanty01@gmail.com>
2025-06-27 17:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let zIndexCounter = 100;
+// script.js
 
-function flipPage(page) {
-  if (!page.classList.contains('flipped')) {
-    page.classList.add('flipped');
-    page.style.zIndex = zIndexCounter--;
+let audioPlayed = false;
+
+function flipPage(element) {
+  // Flip the clicked page
+  element.classList.toggle('flipped');
+
+  // Play music only once on first interaction
+  if (!audioPlayed) {
+    const audio = document.getElementById('bg-music');
+    if (audio) {
+      audio.play().then(() => {
+        audioPlayed = true;
+      }).catch((err) => {
+        console.log('Autoplay blocked:', err);
+      });
+    }
   }
 }
